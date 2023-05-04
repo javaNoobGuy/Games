96fe6e1cf05b258f1346120ddcc3bf10fb6eeaa3
JavaNoobGuy <116377217+javaNoobGuy@users.noreply.github.com>
2023-05-04 11:14:41 -0300

    Add files via upload

--- js/pong.js
+++ js/pong.js
@@ -7,49 +7,17 @@ const screenHeight = 400;
 context.fillStyle = "black";
 context.fillRect(0, 0, screenWidth, screenHeight);
 
-id = setInterval(frame, 20);
-let porcentagem  = 0;
-drawRect(0,0, 22,22, 'blue')
-drawRect(50,80, 56,90, 'cyan')
-function frame(){
-    
-    if(porcentagem >= 1){
-        clearInterval();
-        
-    }else{
+render();
 
-        
-        context.fillStyle = colorTween((porcentagem * 100));;
-        context.beginPath();
-        context.arc(300,350,50,0, (2*Math.PI) * porcentagem, false);
-         context.closePath();
-        context.fill()
-        porcentagem += 0.01;
-        console.log(porcentagem)
-    }
-
-
-}
 
 function render(){
 
     drawRect(0, 0, screenWidth, screenHeight, "black")
+    drawText(50,90, 'score: ', 'white', 33)
 
 
 }
 
-function colorTween(p) {
-    var r1 = 0xff;
-    var g1 = 0xff;
-    var b1 = 0xff;
-    var r2 = 0x99;
-    var g2 = 0x90;
-    var b2 = 0xff;
-    var r3 = (256+(r2-r1)*p/100+r1).toString(16);
-    var g3 = (256+(g2-g1)*p/100+g1).toString(16);
-    var b3 = (256+(b2-b1)*p/100+b1).toString(16);
-    return '#'+r3.substring(1,3)+g3.substring(1,3)+b3.substring(1,3);
-  }
 
 function drawRect(x, y, w, h, color){
 
@@ -69,10 +37,10 @@ function drawCircle(x, y, r, color){
 
 }
 
-function drawText(x, y, text, color){
+function drawText(x, y, text, color, fontSize){
 
     context.fillStyle  = color;
-    context.font = "75px fantasy";
+    context.font = fontSize + "px fantasy";
     context.fillText(text, x, y);
 
 
